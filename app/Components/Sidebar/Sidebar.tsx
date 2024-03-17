58d45572742c9940daab4b99ae702d0aded78e8d
Renato <renato.f.fabri@gmail.com>
2024-03-17 18:35:27 -0300

    Sidebar styles and context

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,11 +1,20 @@
 "use client";
+import { useGlobalState } from "@/app/context/globalProvider";
 import React from "react";
 import styled from "styled-components";
 
 function Sidebar() {
-  return <SidebarStyled>Sidebar</SidebarStyled>;
+  const { theme } = useGlobalState();
+
+  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
 }
 
-const SidebarStyled = styled.nav``;
+const SidebarStyled = styled.nav`
+  width: ${(props) => props.theme.sidebarWidth};
+  position: relative;
+  background-color: ${(props) => props.theme.colorBg2};
+  border-right: 1px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+`;
 
 export default Sidebar;
