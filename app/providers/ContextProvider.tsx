7863de0f3fa4e6fa9636e1cc4662146af3f48fee
Renato <renato.f.fabri@gmail.com>
2024-03-17 14:34:26 -0300

    Sidebar styles and state management

---
+++ app/providers/ContextProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { GlobalProvider } from "../context/globalProvider";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function ContextProvider({ children }: Props) {
+  return (
+    <div>
+      <GlobalProvider>{children}</GlobalProvider>
+    </div>
+  );
+}
+
+export default ContextProvider;
